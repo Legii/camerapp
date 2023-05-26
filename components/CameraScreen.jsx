71ee4b445013f47c0dbaa4fdc3d467744f19a7d8
Legii <62468889+Legii@users.noreply.github.com>
2023-05-26 14:35:07 +0200

    Add files via upload

--- components/CameraScreen.jsx
+++ components/CameraScreen.jsx
@@ -5,15 +5,18 @@ import { Camera, CameraType } from "expo-camera";
 import CircleButton from './CircleButton'
 import * as MediaLibrary from "expo-media-library"
 import { BackHandler } from "react-native"
+import RadioGroup from './RadioGroup';
 
 // create a component
 class CameraScreen extends Component { 
-
+  
     constructor(props) {
         super(props);
         this.state = {
             hasCameraPermission: null,         // przydzielone uprawnienia do używania kamery
-            type: CameraType.back,   // typ kamery
+            type: CameraType.back,
+            settings:[],
+            settingsVissible:false
         };
     }
     componentDidMount(){
@@ -28,6 +31,9 @@ class CameraScreen extends Component {
 
    
     render() {
+        
+        
+
         const { hasCameraPermission } = this.state; // podstawienie zmiennej ze state
         if (hasCameraPermission == null) {
             return <View />;
@@ -41,7 +47,12 @@ class CameraScreen extends Component {
                             this.camera = ref; // Uwaga: referencja do kamery używana później
                         }}
                         style={{ flex: 1 }}
-                        type={this.state.type}>
+                        type={this.state.type}
+                        onCameraReady={() => this.setState({settings:Camera.Constants})}
+           
+                        
+                        
+                        >
                         <View style={{ flex: 1, alignItems:"center", justifyContent:"flex-end" }}>
                             <View style={{flexDirection:"row",alignItems:"center", justifyContent:"flex-end"}}>
                                  <CircleButton text="" onPress={() => {
@@ -57,14 +68,22 @@ class CameraScreen extends Component {
                                  
                                 }
                             }}></CircleButton>
-                            <CircleButton size="small"/>
+                            <CircleButton size="small" onPress={()=> {
+                                if(this.state.settings.length == 0) return
+                            
+                                this.setState({settingsVissible:!this.state.settingsVissible})
+                                
+                            }}/>
+                            {
+                                this.state.settingsVissible && <RadioGroup options={this.state.settings}/>
+                            }
                           </View>
                     
                         </View>
                     </Camera>
                 </View>
             );
-        }
+                        }   
     }
 
 }
--- components/RadioGroup.jsx
+++ components/RadioGroup.jsx
@@ -1,13 +1,27 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-
-const RadioGroup = () => {
-    
+import styles from '../assets/styles'
+import { main_color } from '../assets/styles'
+const RadioGroup = (props) => {
+  
   return (
     <View>
-      <Text>RadioGroup</Text>
+      {
+        Object.keys(props.options).map(key => (
+          <View>
+            <Text style={{color:main_color}}>{key}</Text>
+            {
+              
+             Object.keys(props.options[key]).map((e)=> <Text>A</Text>)
+            }
+          </View>
+        ))
+      }
+
     </View>
+
   )
 }
 
 export default RadioGroup
+//{props.options.map((e) => <Text>{JSON.stringify(e)}</Text>)}
